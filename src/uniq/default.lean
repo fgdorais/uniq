2e9465f2db4304872390d3f3ec7e92506eac4891
François G. Dorais <fgdorais@gmail.com>
2018-07-07 14:52:11 -0400

    fix(uniq): missing copyright header

--- src/uniq/default.lean
+++ src/uniq/default.lean
@@ -1,3 +1,8 @@
+/-
+Copyright © 2018 François G. Dorais. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+
 import .uniq
 import .inverse
 import .bijection
